1aaf09e8774d9cfa7c51eaeea3f105797ef7393b
hamza8nawazi <barkatullahnawazih@gmail.com>
2024-08-20 17:33:28 +0500

    .

--- to do list/index.js
+++ to do list/index.js
@@ -1,35 +1,28 @@
 
 
-const list=[]
-function inputtask(){
-     inputbox = document.getElementById("myInput").value
-     listcontainer = document.getElementById("lists")
-     if(inputbox===""){
-        alert('You must write something')
-     }else{
-        list.push(inputbox)
-        document.getElementById('myInput').value = '';
-        newtodolist= document.createElement("li")
-        newtodolist.innerHTML=inputbox
-        listcontainer.appendChild(newtodolist)
-
-        deletebtn=document.createElement('button')
-        deletebtn.innerHTML="Delete"
-        newtodolist.appendChild(deletebtn)
-        deletebtn.onclick=function(){
-         const index = list.indexOf(inputbox);
-         if (index > -1) {
-         list.splice(index, 1); // Remove the task from the list array
-}
-         listcontainer.removeChild(newtodolist); // Remove the task from the DOM
-
-
+let list = [];
+        let id = 0;
+        function inputtask() {
+            id++;
+            const inputbox = document.getElementById("myInput").value;
+            const listcontainer = document.getElementById("lists");
+            if (inputbox === "") {
+                alert('You must write something');
+            } else {
+                list.push(inputbox);
+                const newtodolist = document.createElement("li");
+                newtodolist.innerHTML = inputbox;
+                const deletebtn = document.createElement('button');
+                deletebtn.innerHTML = "Delete";
+                newtodolist.appendChild(deletebtn);
+                listcontainer.appendChild(newtodolist);
+                document.getElementById('myInput').value = '';
+                deletebtn.onclick = function() {
+                    list = list.filter((item) => item.id !== id);
+                    listcontainer.removeChild(newtodolist);
+               };
+            }
         }
-        
-    }
-    
-}
-
 
 
     
