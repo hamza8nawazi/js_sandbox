e8f7339851572b600febb784303c3919b26eae2d
hamza8nawazi <barkatullahnawazih@gmail.com>
2024-08-22 20:35:12 +0500

    .

--- to do list/index.js
+++ to do list/index.js
@@ -12,18 +12,32 @@ let list = [];
                 list.push(inputbox);
                 const newtodolist = document.createElement("li");
                 newtodolist.innerHTML = inputbox;
+
                 const deletebtn = document.createElement('button');
                 deletebtn.innerHTML = "Delete";
                 newtodolist.appendChild(deletebtn);
+
+                const editbtn = document.createElement('button');
+                editbtn.innerHTML = "Edit";
+                newtodolist.appendChild(editbtn);
+
                 listcontainer.appendChild(newtodolist);
                 document.getElementById('myInput').value = '';
+
                 deletebtn.onclick = function() {
                     list = list.filter((item) => item.id !== id);
                     listcontainer.removeChild(newtodolist);
                };
+
+               editbtn.onclick = function() {                
+                document.getElementById('myInput').value = inputbox
+                list = list.filter((item) => item.id !== id);
+                listcontainer.removeChild(newtodolist);
+            };
+
+           
             }
         }
-
-
+ 
     
 
