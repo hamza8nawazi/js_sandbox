e07d8e93f0f88faeab3adcdc39798cf7a0227d74
hamza8nawazi <barkatullahnawazih@gmail.com>
2024-08-26 17:30:38 +0500

    .

--- to do list/index.js
+++ to do list/index.js
@@ -1,98 +1,59 @@
 
-
-// let list = [];
-//         let id = 0;
-//         function inputtask() {
-//             id++;
-//             const inputbox = document.getElementById("myInput").value;
-//             const listcontainer = document.getElementById("lists");
-//             if (inputbox === "") {
-//                 alert('You must write something');
-//             } else {
-//                 list.push(inputbox);
-//                 const newtodolist = document.createElement("li");
-//                 newtodolist.innerHTML = inputbox;
-
-//                 const deletebtn = document.createElement('button');
-//                 deletebtn.innerHTML = "Delete";
-//                 newtodolist.appendChild(deletebtn);
-
-//                 const editbtn = document.createElement('button');
-//                 editbtn.innerHTML = "Edit";
-//                 newtodolist.appendChild(editbtn);
-
-//                 listcontainer.appendChild(newtodolist);
-//                 document.getElementById('myInput').value = '';
-
-//                 deletebtn.onclick = function() {
-//                     list = list.filter((item) => item.id !== id);
-//                     listcontainer.removeChild(newtodolist);
-//                };
-
-//                editbtn.onclick = function() {                
-//                 document.getElementById('myInput').value = inputbox
-                
-//             };
-
-           
-//             }
-//         }
-const inputbox = document.getElementById("myInput").value;
+const inputbox = document.getElementById("myInput");
 const listcontainer = document.getElementById("lists");
 let list = [];
 let id = 0;
-let editid= null
+let editid = null;
 function inputtask() {
-  const inputbox = document.getElementById("myInput").value;
-  const listcontainer = document.getElementById("lists");
-  // if (inputbox === "") {
-  //   alert("You must write something");
-  //   return;
-  // }
-    id = id + 1;
-    const newItem = { id: id, name: inputbox };
-    list.push(newItem);
-    listcontainer.innerHTML = "";
-    list.map((item) => {
+  if (inputbox.value === "") {
+    alert("You must write something");
+    return;
+  }
+  id = id + 1;
+  const newItem = { id: id, name: inputbox.value };
+  list.push(newItem);
+  console.log(">>>", list);
+  listcontainer.innerHTML = "";
+  list.map((item) => {
     listcontainer.innerHTML += `<li class="item-list">${item.name}
                   <div>
                   <button onclick="DeleteTask(${item.id})">Delete</button>
                   <button onclick="Edit(${item.id})">Edit</button></div>`;
-    });
-  
-    document.getElementById('myInput').value = '';
+  });
+  inputbox.value = "";
 }
-
 function DeleteTask(taskId) {
-  list = list.filter(item => item.id !== taskId);
+  list = list.filter((item) => item.id !== taskId);
   filterdata();
 }
-
-function filterdata(){
-  const listcontainer = document.getElementById("lists");
-  listcontainer.innerHTML = ""; 
+function filterdata() {
+  listcontainer.innerHTML = "";
   list.map((item) => {
     listcontainer.innerHTML += `<li class="item-list">${item.name}
                   <div>
                   <button onclick="DeleteTask(${item.id})">Delete</button>
                   <button onclick="Edit(${item.id})">Edit</button></div>
                   `;
-    });
+  });
 }
-
 function Edit(taskId) {
-  const itemToEdit = list.find(item => item.id === taskId);
-  document.getElementById('myInput').value = itemToEdit.name;
+  let itemToEdit = list.find(item => item.id === taskId);
   editid=taskId;
-  }
-
+  inputbox.value = itemToEdit.name;
+}
 function updatebtn(){
-  const edited = list.find(item => item.id === editid);
-  console.log(edited.name)
-  edited.name=inputbox
+  if (inputbox.value === "") {
+    alert("You must write something");
+    return;
+  }
+  let edited = list.find(item => item.id === editid);
+  edited.name=inputbox.value;
   filterdata();
+  inputbox.value = "";
+ 
 
 }
+
 
 
 
