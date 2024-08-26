638d5c4eb5d936202e44512437973d8f4396a4df
hamza8nawazi <barkatullahnawazih@gmail.com>
2024-08-26 16:31:15 +0500

    .

--- to do list/index.js
+++ to do list/index.js
@@ -37,20 +37,60 @@
            
 //             }
 //         }
- 
+const inputbox = document.getElementById("myInput").value;
+const listcontainer = document.getElementById("lists");
 let list = [];
 let id = 0;
+let editid= null
 function inputtask() {
   const inputbox = document.getElementById("myInput").value;
   const listcontainer = document.getElementById("lists");
-  if (inputbox === "") {
-    alert("You must write something");
-  }
+  // if (inputbox === "") {
+  //   alert("You must write something");
+  //   return;
+  // }
     id = id + 1;
     const newItem = { id: id, name: inputbox };
     list.push(newItem);
     listcontainer.innerHTML = "";
-    console.log(listcontainer);
+    list.map((item) => {
+    listcontainer.innerHTML += `<li class="item-list">${item.name}
+                  <div>
+                  <button onclick="DeleteTask(${item.id})">Delete</button>
+                  <button onclick="Edit(${item.id})">Edit</button></div>`;
+    });
+  
+    document.getElementById('myInput').value = '';
+}
+
+function DeleteTask(taskId) {
+  list = list.filter(item => item.id !== taskId);
+  filterdata();
+}
+
+function filterdata(){
+  const listcontainer = document.getElementById("lists");
+  listcontainer.innerHTML = ""; 
+  list.map((item) => {
+    listcontainer.innerHTML += `<li class="item-list">${item.name}
+                  <div>
+                  <button onclick="DeleteTask(${item.id})">Delete</button>
+                  <button onclick="Edit(${item.id})">Edit</button></div>
+                  `;
+    });
+}
+
+function Edit(taskId) {
+  const itemToEdit = list.find(item => item.id === taskId);
+  document.getElementById('myInput').value = itemToEdit.name;
+  editid=taskId;
+  }
+
+function updatebtn(){
+  const edited = list.find(item => item.id === editid);
+  console.log(edited.name)
+  edited.name=inputbox
+  listcontainer.innerHTML = "";
     list.map((item) => {
     listcontainer.innerHTML += `<li class="item-list">${item.name}
                   <div>
@@ -58,18 +98,9 @@ function inputtask() {
                   <button onclick="Edit(${item.id})">Edit</button></div>`;
     });
     document.getElementById('myInput').value = '';
+
 }
 
-function DeleteTask(taskId) {
-  list = list.filter(item => item.id !== taskId);
-  
-  
-}
-
-function Edit(taskId) {
-  const itemToEdit = list.find(item => item.id === taskId);
-  document.getElementById('myInput').value = itemToEdit.name;
-}
 
 
 
